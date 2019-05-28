2d031d9172d8b66009f0d0c2688e5d9d55b379b4
danghongdang <767478178@qq.com>
2019-05-28 14:18:39 +0800

    Update Test.java
    wangyi music cache to mp3

--- test/src/main/java/com/danghongdang/Test.java
+++ test/src/main/java/com/danghongdang/Test.java
@@ -9,4 +9,42 @@ public class Test {
         String a = "1";
         String sql = "1111" + a;
     }
+    
+    //    public static void main(String[] args) {
+//        DataInputStream dis = null;
+//        DataOutputStream dos = null;
+//        try {
+//            File inFile = new File("/Users/zhanghongzhang/Downloads/26136982-_-_128-_-_19548b2f2bca1e0e62becd274520c89f.uc!");
+//            File outFile = new File("/Users/zhanghongzhang/Downloads/1.mp3");
+//
+//            dis = new DataInputStream(new FileInputStream(inFile));
+//            dos = new DataOutputStream(new FileOutputStream(outFile));
+//            byte[] b = new byte[1024];
+//            int len;
+//            while ((len = dis.read(b)) != -1) {
+//                for (int i = 0; i < len; i++) {
+//                    b[i] ^= 0xa3;
+//                }
+//                dos.write(b, 0, len);
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (dos != null) {
+//                try {
+//                    dos.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            if (dis != null) {
+//                try {
+//                    dis.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
 }
